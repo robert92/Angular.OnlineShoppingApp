67338cf54c1ee26bb6bebf1a9b5502829e00cbb6
Robert_Georgescu <r033rt_05@yahoo.com>
2016-10-14 15:41:07 +0300

    Added Angular

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Robert_Georgescu on 14-Oct-16.
+ */
+
+var appModule = angular.module("shoppingApp", []);
+
+appModule.controller("shoppingController", function($scope) {
+   $scope.testVariable = "blah";
+});
