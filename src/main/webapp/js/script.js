b73e7f9cebf2268c5c79e8c52b5e4dee0e8273f6
Robert_Georgescu <r033rt_05@yahoo.com>
2016-10-20 15:47:56 +0300

    Added Angular functionality to add product to cart

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -6,4 +6,45 @@ var appModule = angular.module("shoppingApp", []);
 
 appModule.controller("shoppingController", function($scope) {
 
+    /*$scope.products = [
+        {
+            name: "abc",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.",
+            price: 3
+        },
+        {
+            name: "uiq",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.",
+            price: 6
+        },
+        {
+            name: "www",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.",
+            price: 4
+        }
+    ];*/
+
+    $scope.products = [];
+    $scope.cartProducts = [];
+
+    $scope.addToCart = function (product) {
+        var cartProduct = getIfAddedToCart(product);
+        if (cartProduct != null) {
+            cartProduct.quantity++;
+        } else {
+            $scope.cartProducts.push({
+                id: product.name,
+                quantity: 1,
+                prod: product
+            });
+        }
+    }
+    
+    function getIfAddedToCart(product) {
+        for(var i=0; i<$scope.cartProducts.length; i++) {
+            if ($scope.cartProducts[i].id == product.name) {
+                return $scope.cartProducts[i];
+            }
+        }
+    }
 });
